026498c7284d782e3e1a7bd9d2d825d8c628bdd1
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-20 14:43:12 -0600

    Manipulando contenido HTML con JavaScript.

---
+++ app.js
@@ -0,0 +1,5 @@
+let titutlo = document.querySelector('h1');
+titutlo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Ingresa un número entre 1 y 10';
