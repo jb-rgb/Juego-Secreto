8897c2b0af606099bf5ccff095e49b6ebe130391
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-21 16:00:59 -0600

    Asignar texto a un elemento HTML.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let titutlo = document.querySelector('h1');
-titutlo.innerHTML = 'Juego del número secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Ingresa un número entre 1 y 10';
+function asignarTextoElemento(elemento, texto) {
+    let ele = document.querySelector(elemento);
+    ele.innerHTML = texto;
+}
 
 function intentoDeUsuario() {
     alert('Funciona');
 }
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Ingresa un número entre 1 y 10');
